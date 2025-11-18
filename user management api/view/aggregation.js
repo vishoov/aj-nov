a0be4aaa32e5bf1683b48678a07f21f0e2592273
Vishoo <vverma971@gmail.com>
2025-11-18 22:40:17 +0530

    upupupupu

--- user management api/view/aggregation.js
+++ user management api/view/aggregation.js
@@ -10,8 +10,7 @@ router.get("/aggregation", async (req, res)=>{
                 $match:{
                     role:"admin"
                 }
-            }
-            
+            }           
         ])
 
         if(!data){
@@ -27,4 +26,75 @@ router.get("/aggregation", async (req, res)=>{
     }
 })
 
+
+//count the number of users, admins, superadmins 
+
+router.get("/roles", async (req, res)=>{
+    try{
+        const analytics = await User.aggregate(
+            [
+                {
+                    $group:{
+                        _id:"$role",
+                        countofUsers:{
+                            $sum:1 
+                        }
+                    }
+                },
+                {
+                    $project:{
+                        "_id":0,
+                        role:"$_id",
+                        countofUsers:1
+                       
+                    }
+                },
+                {
+                    $sort:{
+                        countofUsers:-1 //descending 
+                    }
+                }
+            ]
+        )
+
+        //$sum, $avg, $max,$min
+
+        res.json({
+            analytics
+        })
+
+    }
+    catch(err){
+        res.send(err.message)
+    }
+})
+
+router.get('/pagination/:page', async (req, res)=>{
+    try{
+        const page = req.params.page;
+        const limit = 2;
+
+        const skip = (page-1)*limit
+        // (2-1)*2= 2
+
+        const users = await User.aggregate([
+            {
+                $skip:skip
+            },
+            {
+                $limit:limit
+            }
+        ])
+
+        res.json({users})
+    }
+    catch(err){
+        res.send(err.message)
+    }
+})
+
+
+
 export default router;
+
+// https://www.jwt.io/introduction
